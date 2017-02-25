61566856fdbeb108fd3bc4abadce11a81d36dbaf
neodevelop <juan@makingdevs.com>
2017-02-24 18:31:02 -0600

    Moving logic to the main module Twinder

--- lib/twinder.ex
+++ lib/twinder.ex
@@ -1,2 +1,11 @@
 defmodule Twinder do
+
+  def followers_of(users) when is_list(users) do
+    coordinator = spawn(Twinder.Coordinator, :loop, [ [], Enum.count(users) ])
+    users
+    |> Enum.map(fn u ->
+      pid = spawn(Twinder.User.Followers, :run_async, [])
+      send pid, {coordinator, u}
+    end)
+  end
 end
