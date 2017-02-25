c31a6a109ccf642cfd4bf5352df0ec46dd09fb96
neodevelop <juan@makingdevs.com>
2017-02-25 11:29:39 -0600

    Demo for spawn_link and spawn_monitor, agents, and tasks

---
+++ lib/twinder/process_link_monitor.ex
@@ -0,0 +1,24 @@
+defmodule MyProceses.Linking do
+  def explode, do: exit(:pelaz)
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_link(__MODULE__, :explode, [])
+    receive do
+      {:EXIT, from_pid, reason} ->
+        IO.puts "Exit: #{reason}"
+    end
+  end
+
+  def run2 do
+    {pid, ref} = spawn_monitor(__MODULE__, :explode, [])
+    receive do
+      {:DOWN, ^ref, :process, from_pid, reason} ->
+        IO.puts "Exit: #{reason}"
+    end
+  end
+
+  def double(x) do
+    :timer.sleep 2000
+    x * 2
+  end
+end
