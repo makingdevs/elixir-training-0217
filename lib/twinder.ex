f6dfb55660129d68f1534a54c6310d8b35fdf1b0
neodevelop <juan@makingdevs.com>
2017-03-01 20:00:51 -0600

    Creating an App for a Bank with only one Account

--- lib/twinder.ex
+++ lib/twinder.ex
@@ -1,37 +1,8 @@
 defmodule Twinder do
+  use Application
 
-  def followers_of(users) when is_list(users) do
-    coordinator = spawn(Twinder.Coordinator, :loop, [ [], Enum.count(users), self() ])
-    users
-    |> Enum.map(fn u ->
-      pid = spawn(Twinder.User.Followers, :run_async, [])
-      send pid, {coordinator, u}
-    end)
-    await(self()) # This needs refactor
-  end
-
-  defp await(pid) do
-    receive do
-      {:ok, results, ^pid} -> results
-      _ -> "No message"
-    end
-  end
-
-  def quick_process do
-    self() |> create_and_send |> handle_msg
-  end
-
-  defp create_and_send(parent) do
-    pid = spawn fn ->
-      send parent, {self(), 1+1}
-    end
-    pid
-  end
-
-  defp handle_msg(pid) do
-    receive do
-      {^pid, result} -> result
-    end
+  def start(_type, _args) do
+    Bank.Supervisor.start_link
   end
 
 end
