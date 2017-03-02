b4101323f628798285f76c9c97b00decc9a898ca
neodevelop <juan@makingdevs.com>
2017-03-01 20:01:28 -0600

    Registering the app and adding to the supervisor tree

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Twinder.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [mod: {Twinder,[]}, applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
