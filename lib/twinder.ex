54c167895ce2b7374bbfbe6043465b1afe7a9018
neodevelop <juan@makingdevs.com>
2017-03-03 12:51:00 -0600

    Supervising the supervisor

--- lib/twinder.ex
+++ lib/twinder.ex
@@ -2,7 +2,13 @@ defmodule Twinder do
   use Application
 
   def start(_type, _args) do
-    Bank.Supervisor.start_link
+    import Supervisor.Spec
+
+    children = [
+      supervisor(Bank.Supervisor, [])
+    ]
+
+    Supervisor.start_link(children, [strategy: :one_for_one])
   end
 
 end
