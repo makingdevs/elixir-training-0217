4f7da1c472893e9cb9d7d37917f64d52dc1bf645
neodevelop <juan@makingdevs.com>
2017-03-04 17:04:58 -0600

    Few changes:
    - Adding a worker to make bank operations
      - Adding the supervisor pid
      - Naming the worker
    - Adding module attr to ref supervisor

--- lib/twinder.ex
+++ lib/twinder.ex
@@ -1,12 +1,16 @@
 defmodule Twinder do
   use Application
 
+
+  @bank_account_supervisor Bank.Account.Supervisor
+
   def start(_type, _args) do
     import Supervisor.Spec
 
     children = [
       supervisor(Bank.Supervisor, []),
-      supervisor(Bank.Account.Supervisor, [[name: Bank.Account.Supervisor]])
+      supervisor(Bank.Account.Supervisor, [[name: Bank.Account.Supervisor]]),
+      worker(Bank.Account.Operations, [@bank_account_supervisor, [name: Bank.Account.Operations]])
     ]
 
     Supervisor.start_link(children, [strategy: :one_for_one])
