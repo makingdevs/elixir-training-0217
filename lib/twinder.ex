c5ae46ba704388eb559b16ffb8c65a83ca58cebb
neodevelop <juan@makingdevs.com>
2017-03-05 20:44:16 -0600

    coding #2 removing supervisors to create the new structure...

--- lib/twinder.ex
+++ lib/twinder.ex
@@ -1,16 +1,11 @@
 defmodule Twinder do
   use Application
 
-
-  @bank_account_supervisor Bank.Account.Supervisor
-
   def start(_type, _args) do
     import Supervisor.Spec
 
     children = [
-      supervisor(Bank.Supervisor, []),
       supervisor(Bank.Account.Supervisor, []),
-      worker(Bank.Account.Operations, [@bank_account_supervisor, [name: Bank.Account.Operations]])
       # Implement the Cache with ETS
     ]
 
