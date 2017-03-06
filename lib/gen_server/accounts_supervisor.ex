0292645735ba3209986c269ef05b6015b943af46
neodevelop <juan@makingdevs.com>
2017-03-05 21:39:39 -0600

    #2 improving the `find_account` function and adding `all_accounts` function

--- lib/gen_server/accounts_supervisor.ex
+++ lib/gen_server/accounts_supervisor.ex
@@ -3,18 +3,27 @@ defmodule Bank.Account.Supervisor do
 
   ## Supervisor API
 
-  def new_account(account_number) do
-    Supervisor.start_child __MODULE__, [account_number]
+  def new_account do
+    Supervisor.start_child __MODULE__, [:os.system_time(:millisecond) ]
   end
 
   def find_account(account_number) do
-    __MODULE__
-    |> Supervisor.which_children
+    childrens()
     |> Enum.find( fn {_, a, _, _} ->
       # Search in GenServer
+      Bank.Account.Operations.info(a) == account_number
     end)
     |> elem(1) # {_, pid, _, _}
   end
+
+  def all_accounts do
+    childrens()
+    |> Enum.map( fn {_, a, _, _} ->
+      Bank.Account.Operations.info(a)
+    end)
+  end
+
+  defp childrens, do: __MODULE__ |> Supervisor.which_children
 
   ## Supervisor Callbacks
 
