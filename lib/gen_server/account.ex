141c32aa72d06802e69edce6ce94e6303abf9cac
neodevelop <juan@makingdevs.com>
2017-03-11 15:51:20 -0600

    coding #4 Modifyng the account agent to store a history of movements

--- lib/gen_server/account.ex
+++ lib/gen_server/account.ex
@@ -1,22 +1,23 @@
 defmodule Bank.Account do
 
-  def start_link do
-    Agent.start_link fn -> {:os.system_time(:millisecond) , 0} end
+  def start_link(account_number) do
+    Agent.start_link fn -> {account_number , []} end
   end
 
   def deposit(account, amount) do
-    Agent.update(account, fn {account_number, balance} ->
-      {account_number, balance + amount}
+    Agent.update(account, fn {account_number, movements} ->
+      {account_number, [ {:deposit, amount} | movements ] }
     end)
   end
 
   def withdraw(account, amount) do
-    Agent.update(account, fn {account_number, balance} ->
-      {account_number, balance - amount}
+    Agent.update(account, fn {account_number, movements} ->
+      {account_number, [ {:withdraw, amount} | movements ] }
     end)
   end
 
   def balance(account) do
+    # Todo: do the calculation
     Agent.get account, &(&1)
   end
 
