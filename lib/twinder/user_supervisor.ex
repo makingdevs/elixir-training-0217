f972e48d408e496b5d32856422e7d1c8eed2d328
neodevelop <juan@makingdevs.com>
2017-03-11 17:41:11 -0600

    #1 adding a supervisor to all users

---
+++ lib/twinder/user_supervisor.ex
@@ -0,0 +1,29 @@
+defmodule Twinder.User.Supervisor do
+  use Supervisor
+
+  ## Supervisor API
+
+  def new_user(username) do
+    Supervisor.start_child __MODULE__, [{0, username}]
+  end
+
+  def all_accounts do
+    childrens()
+  end
+
+  defp childrens, do: __MODULE__ |> Supervisor.which_children
+
+  ## Supervisor callbacks
+
+  def start_link do
+    Supervisor.start_link __MODULE__, :ok, name: __MODULE__
+  end
+
+  def init(:ok) do
+    children = [
+      worker(Twinder.User.Store, [])
+    ]
+    supervise(children, strategy: :simple_one_for_one)
+  end
+
+end
