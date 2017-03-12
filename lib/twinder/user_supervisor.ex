e2e1015bfdb4b235d98b101358b83a6b66b9a083
neodevelop <juan@makingdevs.com>
2017-03-11 23:21:23 -0600

    #1 Using an agent instead of genserver, but still is blocked by two request at the same time

--- lib/twinder/user_supervisor.ex
+++ lib/twinder/user_supervisor.ex
@@ -5,12 +5,16 @@ defmodule Twinder.User.Supervisor do
 
   def new_user(username) do
     Task.Supervisor.start_child(Twinder.TaskSupervisor, fn ->
+      #Supervisor.start_child __MODULE__, [username]
       Supervisor.start_child __MODULE__, [username]
     end)
   end
 
   def all_accounts do
     childrens()
+    |> Enum.map(fn {_, pid, _, _} ->
+      Twinder.User.Store.current_state pid
+    end)
   end
 
   defp childrens, do: __MODULE__ |> Supervisor.which_children
@@ -23,7 +27,8 @@ defmodule Twinder.User.Supervisor do
 
   def init(:ok) do
     children = [
-      worker(Twinder.User.Server, [])
+      #worker(Twinder.User.Server, [])
+      worker(Twinder.User.Store, [])
     ]
     supervise(children, strategy: :simple_one_for_one)
   end
