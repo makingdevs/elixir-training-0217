2eb97d504141469b8f219a3e688bb29b66f8cf4f
neodevelop <juan@makingdevs.com>
2017-03-11 18:34:21 -0600

    #1 Adding features to user module to extract the info from Github

--- lib/twinder/user.ex
+++ lib/twinder/user.ex
@@ -1,11 +1,45 @@
 defmodule Twinder.User do
-  defstruct username: "", id: 0, followers: []
+  import JSON, only: [decode: 1]
+  alias Twinder.User
+  alias HTTPoison, as: HTTP
+  alias HTTP.Response
 
-  def new(id, username) do
-    %__MODULE__{id: id, username: username}
+  defstruct username: "", name: "", id: 0, followers: []
+
+  @user_url "https://api.github.com/users/:username"
+  @access_token Application.get_env(:twinder, :access_token)
+  @headers ["Authorization": "token #{@access_token}"]
+  @http_options [ssl: [{:versions, [:'tlsv1.2']}], recv_timeout: 1000]
+
+  def get_user(username) do
+    username
+    |> create_url
+    |> make_a_request
+    |> parse_response
+    |> create_new_user
   end
 
-  def capitalize_username(%Twinder.User{ username: username}) do
-    String.capitalize username
+  defp create_url(username) do
+    import String, only: [replace: 3]
+    @user_url
+    |> replace(":username", username)
   end
+
+  defp make_a_request(url) do
+    HTTP.get url, @headers, @http_options
+  end
+
+  defp parse_response({:ok, %Response{
+                          body: body, headers: _headers, status_code: 200}}) do
+    body |> decode
+  end
+  defp parse_response({:ok, %Response{
+                          body: body, headers: _headers, status_code: code}}) when code in 400..499 do
+    body |> decode
+  end
+
+  defp create_new_user({:ok, user_info}) do
+    %__MODULE__{id: user_info["id"], username: user_info["login"], name: user_info["name"]}
+  end
+
 end
