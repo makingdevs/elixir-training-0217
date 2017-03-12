28cd6db610f325913afb4ba43565dae7e390f1c0
neodevelop <juan@makingdevs.com>
2017-03-11 23:46:24 -0600

    #1 Making the `followers_of` a task for the current process

--- lib/twinder/user_store.ex
+++ lib/twinder/user_store.ex
@@ -4,7 +4,11 @@ defmodule Twinder.User.Store do
   ## We have to make a Task to process
   def start_link(username) do
     Agent.start_link fn ->
-      Twinder.User.get_user username
+      followers = Task.Supervisor.async(Twinder.TaskSupervisor, fn ->
+        Twinder.User.Followers.followers_of username
+      end) |> Task.await
+      %Twinder.User{ Twinder.User.get_user(username) |
+                    followers: followers }
     end
   end
 
