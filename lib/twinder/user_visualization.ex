603867c0e558555606c6de8bdbe053be7e61455f
neodevelop <juan@makingdevs.com>
2017-03-12 20:23:02 -0600

    #6 Running all the process in a single function, needs refactor!

--- lib/twinder/user_visualization.ex
+++ lib/twinder/user_visualization.ex
@@ -1,5 +1,15 @@
 defmodule Twinder.User.Visualization do
-  alias Graphvix.{Graph, Node, Edge, Cluster}
+  alias Graphvix.{Graph, Node, Edge}
+
+  def find_followers_relations_between(username1, username2) do
+    Twinder.User.Supervisor.new_users([username1, username2])
+    common_usernames = Twinder.User.Supervisor.common_followers username1, username2
+    Twinder.User.Supervisor.new_users(common_usernames)
+    common_followers = common_usernames |> Twinder.User.Supervisor.find_many
+    interactions = common_followers |> Twinder.User.Supervisor.find_interactions
+    following_interactions = interactions |> Enum.filter(fn {_, t, _} -> t == :following end)
+    show_interactions(following_interactions)
+  end
 
   def show_interactions(interactions) do
     Graph.new(:interactions)
