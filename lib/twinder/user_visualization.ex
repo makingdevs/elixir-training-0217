2833cf34500f832fac6b72c92eb885d4ec75c00d
neodevelop <juan@makingdevs.com>
2017-03-12 20:21:46 -0600

    #6 adding some helper functions, and returning a list of strings in `common_followers`

--- lib/twinder/user_visualization.ex
+++ lib/twinder/user_visualization.ex
@@ -3,18 +3,27 @@ defmodule Twinder.User.Visualization do
 
   def show_interactions(interactions) do
     Graph.new(:interactions)
+    graph_names = interactions
+    |> Enum.map(fn {u1, _, u2} -> [u1, u2] end)
+    |> Enum.flat_map(&(&1))
+    |> Enum.uniq
+
+    graph_nodes = graph_names
+    |> Enum.map(&({&1, Node.new(label: &1)}))
+
     interactions
-    |> create_graph
+    |> create_graph_with_nodes(graph_nodes)
+
     Graph.save
     Graph.compile(:png)
   end
 
-  defp create_graph(interactions) do
+  defp create_graph_with_nodes(interactions, graph_nodes) do
     interactions
     |> Enum.each(fn {u1, _interaction, u2} ->
-      {node1_id, _node} = Node.new(label: u1)
-      {node2_id, _node} = Node.new(label: u2)
-      {edge_id, _edge} = Edge.new(node1_id, node2_id)
+      {_, {node1_id, _node}} = graph_nodes |> Enum.find(fn {u, _} -> u == u1 end)
+      {_, {node2_id, _node}} = graph_nodes |> Enum.find(fn {u, _} -> u == u2 end)
+      {_edge_id, _edge} = Edge.new(node1_id, node2_id)
     end)
   end
 
