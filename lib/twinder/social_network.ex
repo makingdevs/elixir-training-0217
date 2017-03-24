baabff1348dcdfa57034bf540cd4458210a41eac
neodevelop <juan@makingdevs.com>
2017-03-23 21:06:31 -0600

    #8 adding behaviour for the followers of user

--- lib/twinder/social_network.ex
+++ lib/twinder/social_network.ex
@@ -3,3 +3,9 @@ defmodule Twinder.Social.User do
   @callback get_user(String.t) :: Twinder.User.t
 
 end
+
+defmodule Twinder.Social.User.Followers do
+
+  @callback followers_of(Twinder.User.t) :: [Twinder.User.t]
+
+end
