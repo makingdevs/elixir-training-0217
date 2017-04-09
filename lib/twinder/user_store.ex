e95ab009f88c9c43eabb94b926e6337efe387dc3
neodevelop <juan@makingdevs.com>
2017-04-09 18:53:07 -0500

    #9 Adjusting the store to make differences between social networks

--- lib/twinder/user_store.ex
+++ lib/twinder/user_store.ex
@@ -1,13 +1,13 @@
 defmodule Twinder.User.Store do
   alias Twinder.Social.User.Github
+  alias Twinder.Social.User.Twitter
 
-  def start_link(username) do
+  def start_link(username, network) do
     Agent.start_link fn ->
-      user = Github.get_user(username)
-      followers = Task.Supervisor.async(Twinder.TaskSupervisor, fn ->
-        Github.Followers.followers_of user
-      end) |> Task.await
-      %Twinder.User{ user | followers: followers }
+      case network do
+        :twitter -> start_link_for_github(username)
+        :github -> start_link_for_twitter(username)
+      end
     end
   end
 
@@ -17,6 +17,22 @@ defmodule Twinder.User.Store do
 
   def current_state(user) do
     Agent.get user, &(&1)
+  end
+
+  defp start_link_for_github(username) do
+    user = Github.get_user(username)
+    followers = Task.Supervisor.async(Twinder.TaskSupervisor, fn ->
+      Github.Followers.followers_of user
+    end) |> Task.await
+    %Twinder.User{ user | followers: followers }
+  end
+
+  defp start_link_for_twitter(username) do
+    user = Twitter.get_user(username)
+    followers = Task.Supervisor.async(Twinder.TaskSupervisor, fn ->
+      Twitter.Followers.followers_of user
+    end) |> Task.await
+    %Twinder.User{ user | followers: followers }
   end
 
 end
